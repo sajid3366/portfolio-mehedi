b1d5a63c4b2e013d716e74a32275323343d92c0b
sajid360 <ahteshamsajid8@gmail.com>
2024-05-15 00:21:24 +0600

    routing added for all the routes

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,11 +1,42 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../Layout/Root";
+import Home from "../components/Home/Home";
+import About from "../components/About/About";
+import Service from "../components/Service/Service";
+import Work from "../components/Work/Work";
+import Blog from "../components/Blog/Blog";
+import Contact from "../components/Contact/Contact";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Root></Root>
-        
+        element: <Root></Root>,
+        children:[
+            {
+                path: "/",
+                element: <Home></Home>
+            },
+            {
+                path: "/about",
+                element: <About></About>
+            },
+            {
+                path: "/service",
+                element: <Service></Service>
+            },
+            {
+                path: "/work",
+                element: <Work></Work>
+            },
+            {
+                path: "/blog",
+                element: <Blog></Blog>
+            },
+            {
+                path: "/contact",
+                element: <Contact></Contact>
+            },
+        ],
     }
 ])
 
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h1>This is from home</h1>
+        </div>
+    );
+};
+
+export default Home;
